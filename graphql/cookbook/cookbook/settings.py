dcc9c25e7f75e1a9b6d12917d19fd2bc3b010af1
Mohamad Aldawood <dawood939m@gmail.com>
2022-12-14 16:41:24 +0400

    Update settings.py

--- graphql/cookbook/cookbook/settings.py
+++ graphql/cookbook/cookbook/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ka3io^0vt_%gw)&0dq1iofn=k-*8+5rm@ve(afh@88g1$9*)79'
+SECRET_KEY = 'YOUR SECRET KEY HERE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
